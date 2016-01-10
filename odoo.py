339b7bf45a7208074fc186021f796fc2c1e67789
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2016-01-10 14:03:27 +0100

    futurize -1 root dir

--- odoo.py
+++ odoo.py
@@ -13,6 +13,7 @@
 # subclassing the Command object
 #
 #----------------------------------------------------------
+from __future__ import print_function
 import os
 import re
 import sys
@@ -28,7 +29,7 @@ if re.search('github.com[:/]odoo/odoo.git$', sys.argv[2]):
 """
 
 def printf(f,*l):
-    print "odoo:" + f % l
+    print("odoo:" + f % l)
 
 def run(*l):
     if isinstance(l[0], list):
@@ -76,7 +77,7 @@ def cmd_setup_git():
         run('git','config','alias.pl','pull --ff-only')
         pre_push_path = os.path.join(git_dir, '.git/hooks/pre-push')
         open(pre_push_path,'w').write(GIT_HOOKS_PRE_PUSH.strip())
-        os.chmod(pre_push_path, 0755)
+        os.chmod(pre_push_path, 0o755)
         # setup odoo remote
         run('git','config','remote.odoo.url','https://github.com/odoo/odoo.git')
         run('git','config','remote.odoo.pushurl','git@github.com:odoo/odoo.git')
